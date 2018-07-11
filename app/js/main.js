de368067cfe814460b6100c0e5f9bfc1b5d24378
Paola Vargas <vrgspaola68@gmail.com>
2018-07-11 18:48:09 -0400

    Responsive y hovers

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,138 @@
-$('#blog, #gallery, #control, #create').hover(function(ev){
-  console.log(ev)
-}, function(ev){
-  console.log(ev)
+// ABOUT HOVER
+$('#control').hover(function(ev) {
+
+  var hover = ['blog','gallery','create'];
+
+  $.each( hover, function(index, val) {
+    $('.icon-' + val).addClass('icon-' + val + '_hover').removeClass('icon-' + val);
+    $('#' + val + ' ' + 'h1').addClass('card-title_hover');
+  });
+
+  // Mouse leave
+  $('#control').mouseleave(function() {
+    $.each( hover, function(index, value) {
+      $('.icon-' + value + '_hover').addClass('icon-' + value).removeClass('icon-' + value + '_hover');
+      $('#' + value + ' ' + 'h1').removeClass('card-title_hover');
+    });
+  });
 })
 
+$('#blog').hover(function(ev) {
+
+  var hover = ['control','gallery','create'];
+
+  $.each( hover, function(index, val) {
+    $('.icon-' + val).addClass('icon-' + val + '_hover').removeClass('icon-' + val);
+    $('#' + val + ' ' + 'h1').addClass('card-title_hover');
+  });
+
+  // Mouse leave
+  $('#blog').mouseleave(function() {
+    $.each( hover, function(index, value) {
+      $('.icon-' + value + '_hover').addClass('icon-' + value).removeClass('icon-' + value + '_hover');
+      $('#' + value + ' ' + 'h1').removeClass('card-title_hover');
+    });
+  });
+})
+
+$('#gallery').hover(function(ev) {
+
+  var hover = ['blog','control','create'];
+
+  $.each( hover, function(index, val) {
+    $('.icon-' + val).addClass('icon-' + val + '_hover').removeClass('icon-' + val);
+    $('#' + val + ' ' + 'h1').addClass('card-title_hover');
+  });
+
+  // Mouse leave
+  $('#gallery').mouseleave(function() {
+    $.each( hover, function(index, value) {
+      $('.icon-' + value + '_hover').addClass('icon-' + value).removeClass('icon-' + value + '_hover');
+      $('#' + value + ' ' + 'h1').removeClass('card-title_hover');
+    });
+  });
+})
+
+$('#create').hover(function(ev) {
+
+  var hover = ['blog','gallery','control'];
+
+  $.each( hover, function(index, val) {
+    $('.icon-' + val).addClass('icon-' + val + '_hover').removeClass('icon-' + val);
+    $('#' + val + ' ' + 'h1').addClass('card-title_hover');
+  });
+
+  // Mouse leave
+  $('#create').mouseleave(function() {
+    $.each( hover, function(index, value) {
+      $('.icon-' + value + '_hover').addClass('icon-' + value).removeClass('icon-' + value + '_hover');
+      $('#' + value + ' ' + 'h1').removeClass('card-title_hover');
+    });
+  });
+})
+
+//FEATURES HOVER
+$('#red').hover(function(ev) {
+  var features = ['light-blue','dark-blue','dark-red'];
+
+  $.each( features, function(index, a) {
+    $('#' + a).addClass('feature_gray');
+  });
+
+  // Mouse leave
+  $('#red').mouseleave(function() {
+    $.each( features, function(index, r) {
+      $('#' + r).removeClass('feature_gray');
+    });
+  });
+})
+
+$('#light-blue').hover(function(ev) {
+  var features = ['red','dark-blue','dark-red'];
+
+  $.each( features, function(index, a) {
+    $('#' + a).addClass('feature_gray');
+  });
+
+  // Mouse leave
+  $('#light-blue').mouseleave(function() {
+    $.each( features, function(index, r) {
+      $('#' + r).removeClass('feature_gray');
+    });
+  });
+})
+
+$('#dark-blue').hover(function(ev) {
+  var features = ['red','light-blue','dark-red'];
+
+  $.each( features, function(index, a) {
+    $('#' + a).addClass('feature_gray');
+  });
+
+  // Mouse leave
+  $('#dark-blue').mouseleave(function() {
+    $.each( features, function(index, r) {
+      $('#' + r).removeClass('feature_gray');
+    });
+  });
+})
+
+$('#dark-red').hover(function(ev) {
+  var features = ['red','dark-blue','light-blue'];
+
+  $.each( features, function(index, a) {
+    $('#' + a).addClass('feature_gray');
+  });
+
+  // Mouse leave
+  $('#dark-red').mouseleave(function() {
+    $.each( features, function(index, r) {
+      $('#' + r).removeClass('feature_gray');
+    });
+  });
+})
+
+// SLIDERS
 $('.slider-for').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
@@ -20,118 +149,3 @@ $('.slider-nav').slick({
   centerMode: true,
   focusOnSelect: true
 });
-
-
-var particles = {
-  "particles": {
-    "number": {
-      "value": 160,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#15aeda"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 1,
-      "random": true,
-      "anim": {
-        "enable": true,
-        "speed": 1,
-        "opacity_min": 0,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 4,
-        "size_min": 0.3,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 150,
-      "color": "#15aeda",
-      "opacity": 0.4,
-      "width": 1
-    },
-    "move": {
-      "enable": true,
-      "speed": 0.17,
-      "direction": "none",
-      "random": true,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 600
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "bubble"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "resize": false
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 250,
-        "size": 0,
-        "duration": 2,
-        "opacity": 0,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 400,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
- };
- particlesJS('particles-js', particles, function() {
-   console.log('callback - particles.js config loaded');
- });
