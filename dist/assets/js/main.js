bc5a3151a24dcfff3cf3ee9635df4b9387ccf5fa
Paola Vargas <vrgspaola68@gmail.com>
2018-08-13 17:03:49 -0400

    Home details + particles animation

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -75,3 +75,22 @@ $('#return-to-top').click(function() {      // When arrow is clicked
       scrollTop : 0                       // Scroll to top of body
   }, 500);
 });
+
+
+function particles() {
+  $.each($(".particles"), function(){
+     var bubblecount = ($(this).width()/50)*6;
+     for(var i = 0; i <= bubblecount; i++) {
+        var size = ($.rnd(30,40)/10);
+        $(this).append('<span class="particle" style="top:' + $.rnd(5,90) + '%; left:' + $.rnd(0,95) + '%;width:' + size + 'px; height:' + size + 'px;animation-delay: ' + ($.rnd(0,30)/10) + 's;"></span>');
+     }
+  });
+}
+
+jQuery.rnd = function(m,n) {
+  m = parseInt(m);
+  n = parseInt(n);
+  return Math.floor( Math.random() * (n - m + 1) ) + m;
+}
+
+particles();
