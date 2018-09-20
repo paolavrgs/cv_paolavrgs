f1be7bb2cfe7e0822a4f83b6dc7f3081e7282dc8
Paola Vargas <vrgspaola68@gmail.com>
2018-09-20 11:57:42 -0400

    Gifs changed by videos - Frontend spanish version DONE

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,7 @@ gulp.task('add_materialize', function(){
 
 //Save cache and minify images
 gulp.task('images', function(){
-  return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg)')
+  return gulp.src('app/img/**/*.+(png|jpg|jpeg|gif|svg|mp4|m4v)')
   // Caching images that ran through imagemin
   .pipe(cache(imagemin({
       interlaced: true
