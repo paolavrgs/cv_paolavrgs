826acda3cf9bc7123c159dfbe7342df724d430f3
Paola Vargas <vrgspaola68@gmail.com>
2020-06-03 17:49:15 -0400

    Add new projects, remove progress lines for professional skills, modify personal info, work experience and education.

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,4 @@
-const qtrf = {
-  company_name: 'QTRF Net Consulting C.A',
-  year: 'January 2018',
-  type: 'Web',
-  description: 'Lorem ipsum...',
-  video: 'assets/img/qtrf.mp4'
-}
-
-const keppler = {
-  company_name: 'Keppler Admin',
-  year: 'August 2018',
-  type: 'App',
-  description: 'Lorem ipsum...',
-  video: ''
-}
-
-const prodesa = {
-  company_name: 'Prodesa',
-  year: 'December 2017',
-  type: 'Web',
-  description: 'Lorem ipsum...',
-  video: ''
-}
-
-const project = $('.item a')
-var arr = []
-for (let i=0; i<project.length; i++) {
-  arr.push(project[i].dataset.project)
-}
-// console.log(arr)
-
-
-$('.modal').on('shown.bs.modal', function () {
+$('.modal#keppleradmin').on('shown.bs.modal', function () {
   $('.single-item').slick({
     autoplay: true,
     autoplaySpeed: 2000,
@@ -44,11 +12,3 @@ WebFont.load({
     families: ['Quicksand:400,500,700']
   }
 });
-
-$('.personal-header img').mouseenter(function() {
-  $('.personal-header img').attr('src','assets/img/profile_hover.webp');
-})
-
-$('.personal-header img').mouseleave(function() {
-  $('.personal-header img').attr('src','assets/img/profile.webp');
-})
